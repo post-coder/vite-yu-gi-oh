8b6e3efb1f5bf16e4cd2e9b2146b9051a59c5517
post-coder <gabrielspanu.dev@gmail.com>
2024-03-05 09:41:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import {store} from './store';
+
+export default {
+  data() {
+    return {
+
+      store,
+    }
+  },
+
+  created() {
+
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      .then(res => {
+        console.log(res.data.data)
+        this.store.cards = res.data.data;
+      })
+
+  },
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lanc="scss">
+
+</style>
